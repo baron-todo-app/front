ad7ee08e7cda502dbb8d375a77a267d046deca2d
baronTommy <16768208+baronTommy@users.noreply.github.com>
2019-04-26 21:17:55 +0900

    fix

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,7 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  const req = require.context("../src", true, /.stories.(tsx)$/);
+  const req = require.context("../src/components", true, /.stories.(tsx)$/);
   req.keys().forEach(filename => req(filename));
 }
 
